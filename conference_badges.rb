9b92684755222934e1c702d2219153c2389cb036
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-03-16 15:49:58 -0700

    commit tree 2da17b371cbe74a505d160ff05a27a1dcb57bc67

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+    arr.map {|name| "Hello, my name is #{name}."}
+end
+
+def assign_rooms(arr)
+   rooms = []
+   arr.each_with_index {|name, index| rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+   rooms
+end
+
+def printer(arr)
+    batch_badge_creator(arr).each {|hello| puts hello}
+    assign_rooms(arr).each {|hello| puts hello}    
+end  
